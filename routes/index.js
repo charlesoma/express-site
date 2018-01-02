9ca67980c852b442337e5e5949e4b870a5415ad8
unknown <charlyoma007@gmail.com>
2018-01-03 00:55:39 +0100

    layout view created

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home | Basic Express Site' });
 });
+
+router.get('/users', function(req, res, next) {
+	res.render('users', { title: 'Users Page'});
+})
 
 module.exports = router;
